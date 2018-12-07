280e07e56735e4a2634c5ef5d690fe437d3e9d7e
Caleb Wells <Caleb.Wells@alaskaair.com>
2018-12-07 13:17:38 -0800

    adding to moviescontroller

--- Vidly/Vidly/Controllers/MoviesController.cs
+++ Vidly/Vidly/Controllers/MoviesController.cs
@@ -13,7 +13,29 @@ namespace Vidly.Controllers
         public ActionResult Random()
         {
             var movie = new Movie() { Name = "Shrek" };
-            return View(movie);
+            //return View(movie);
+            return RedirectToAction("Index", "Home", new { page = 1, sortBy = "Name" });
+        }
+
+        public ActionResult Edit(int movieId)
+        {
+            return Content("movieId=" + movieId);
+        }
+
+        public ActionResult Index(int? pageIndex, string sortBy)
+        {
+            if (!pageIndex.HasValue)
+                pageIndex = 1;
+            if (String.IsNullOrWhiteSpace(sortBy))
+                sortBy = "Name";
+
+            return Content(String.Format("pageIndex={0}&sortBy={1}", pageIndex, sortBy));
+        }
+
+        public ActionResult ByReleaseDate(int year, int month)
+        {
+            return Content($"{year}/{month}");
+
         }
     }
 }
